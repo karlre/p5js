54398f93f200aa5f2cbe5c30338f775e7992058d
jaegr <karlre@gmail.com>
2016-09-10 01:38:21 +0200

    added 2-order chain

--- markov/sketch.js
+++ markov/sketch.js
@@ -15,14 +15,16 @@ function draw() {
 }
 
 function process(corpus) {
-    current = "I";
+    current = "I am";
     output = current;
     for (var i = 0; i < 100; i++) {
         var idx = floor(random(corpus[current].length));
         next = corpus[current][idx];
+        console.log(next);
         output += " " + next;
-        current = next;
+        current = current.split(" ")[1] + " " + next;
     }
+    output += "!";
     var span = createDiv(output);
     span.style("border: 1px solid black; border-radius: 5px; margin-top: 100px; float: left; width: 400px")
     noLoop();
