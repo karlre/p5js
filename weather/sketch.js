759f4b575c76960d51f774e64e1bc8ad32c32e07
Kalle <karl.re@gmail.com>
2016-09-27 12:43:42 +0200

    remove debug logs

--- weather/sketch.js
+++ weather/sketch.js
@@ -66,7 +66,6 @@ function gotWeather(weather) {
     if (weather.response.error) {
         createP("City not found!")
     } else {
-        console.log(weather);
         wind_dir = weather.current_observation.wind_degrees;
         wind_speed = weather.current_observation.wind_kph;
     }
